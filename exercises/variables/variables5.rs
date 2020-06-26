5fe1d780dc4f106de76cc4db6920d686478e96a0
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-06-26 21:51:20 +0545

    implementation of shadowing concept

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,18 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let number = "3"; // don't change this line
     println!("Number {}", number);
-    number = 3;
+    
+    // We need to implement the shadowing concept here. Shadowing allows you 
+    // to define the variable with existing name along with different data type
+    {
+        
+    let number: i32 = 3;
+
+    }
     println!("Number {}", number);
 }
