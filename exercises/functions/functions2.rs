5a58cfa87c94336af95aa3f8ac429c34a37af7df
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-06-27 07:50:07 +0545

    give a return type to the argument of the function

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,14 @@
 // functions2.rs
 // Make me compile! Execute `rustlings hint functions2` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     call_me(3);
 }
 
-fn call_me(num) {
+// Insert the datatype of the num variable passed into the call_me function. 
+fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
