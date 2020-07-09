4fc75bfac782cd926b38bcb913518a5f197c914c
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-07-09 06:08:18 +0545

    implement your macro rules

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -4,8 +4,12 @@
 // - Macros
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
-
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($val:expr) => {
+        format!("Hello {}",$val);
+    };
+}
+// I AM DONE
 
 #[cfg(test)]
 mod tests {
